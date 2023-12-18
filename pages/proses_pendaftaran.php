048402e03ee664aa72bedb0dbf3f1be911660ba0
unknown <rivaldialwi.com>
2023-12-18 08:50:47 +0700

    Menambahkan jeda saat proses boking

--- pages/proses_pendaftaran.php
+++ pages/proses_pendaftaran.php
@@ -14,54 +14,61 @@ $nomor_plat = $_POST['nomor_plat'];
 $total_biaya = $_POST['total_biaya'];
 $tgl_pendaftaran = $_POST['tgl_pendaftaran'];
 $tgl_boking = $_POST['tgl_boking'];
-date_default_timezone_set('Asia/Jakarta');
-// $jam_pendaftaran=date("H:i:s");
 $jam_pendaftaran = $_POST['jam_pendaftaran'];
 
-$cek = mysql_query("SELECT COUNT(id_pendaftaran) AS jumlah_daftar FROM pendaftaran WHERE tgl_boking = '$tgl_boking' AND jam_pendaftaran = '$jam_pendaftaran'");
-$htg = mysql_fetch_array($cek);
-$jumlahnya = $htg['jumlah_daftar'];
-if ($jumlahnya != 0) {
-?>
-	<script language="JavaScript">
-		alert('Maaf, Jam Dan Tanggal Yang Anda Inginkan Sudah Dibooking, Silahkan Pilih Jam Dan Tanggal Yang Lainnya');
-		document.location = '../index.php'
-	</script>
-	<?php
+date_default_timezone_set('Asia/Jakarta');
+
+// Hitung waktu selesai dengan menambahkan 30 menit ke waktu booking yang dipilih
+$end_time = date('H:i:s', strtotime($jam_pendaftaran . ' +30 minutes'));
+
+// Periksa apakah ada booking yang tumpang tindih dalam rentang waktu yang dipilih
+$check_overlap_query = "SELECT COUNT(id_pendaftaran) AS jumlah_daftar FROM pendaftaran 
+                        WHERE tgl_boking = '$tgl_boking' 
+                        AND ((jam_pendaftaran >= '$jam_pendaftaran' AND jam_pendaftaran < '$end_time') 
+                             OR (jam_pendaftaran < '$jam_pendaftaran' AND '$jam_pendaftaran' < ADDTIME(jam_pendaftaran, '00:30:00')))";
+
+$overlap_result = mysql_query($check_overlap_query);
+$overlap_data = mysql_fetch_array($overlap_result);
+$overlap_count = $overlap_data['jumlah_daftar'];
+
+if ($overlap_count != 0) {
+    ?>
+    <script language="JavaScript">
+        alert('Maaf, Jam Yang Anda Pilih Sudah Dibooking atau Tidak Tersedia, Silahkan Pilih Jam Lainnya');
+        document.location = '../index.php';
+    </script>
+    <?php
 } else {
+    // Kode Anda yang sudah ada untuk memproses pendaftaran
+    if ($next > '20') {
+        ?>
+        <script language="JavaScript">
+            alert('Maaf, Antrian Hari Ini Sudah Penuh, Silahkan Daftar Kembali Di Hari Berikutnya');
+            document.location = '../index.php';
+        </script>
+    <?php
+    } elseif ($next <= '30') {
+        $queryy = "insert into customer(id_customer, nama, no_hp, alamat, nomor_plat, type_mobil) values('$id_customer','$nama', '$no_hp', '$alamat', '$nomor_plat', '$type_mobil')";
+        $hasill = mysql_query($queryy);
 
-	if ($next > '30') {
-	?>
-		<script language="JavaScript">
-			alert('Maaf, Antrian Hari Ini Sudah Penuh, Silahkan Daftar Kembali Di Hari Berikutnya');
-			document.location = '../index.php'
-		</script>
-
-	<?php
-	} elseif ($next <= '30') {
-
-		$queryy = "insert into customer(id_customer, nama, no_hp, alamat, nomor_plat, type_mobil) values('$id_customer','$nama', '$no_hp', '$alamat', '$nomor_plat', '$type_mobil')";
-		$hasill = mysql_query($queryy);
-
-		$query = "insert into pendaftaran(id_pendaftaran, no_antrian, id_customer, id_jenis_service, tgl_pendaftaran, jam_pendaftaran, tgl_boking, total_biaya, status) values(NULL,'$no_antrian', '$id_customer', '$id_jenis_service', '$tgl_pendaftaran', '$jam_pendaftaran', '$tgl_boking', '$total_biaya', 'Pendaftaran')";
-		$hasil = mysql_query($query);
-	}
-	//see the result
-	if ($hasil) {
-	?>
-		<script language="JavaScript">
-			alert('Pendaftaran Anda Berhasil Dilakukan, Silahkan Datang Ke service Kami');
-			window.location.replace('../index.php');
-		</script>
-
-	<?php
-	} else {
-	?>
-		<script language="JavaScript">
-			alert('Pendaftaran Gagal Dilakukan');
-			document.location = '../index.php'
-		</script>
+        $query = "insert into pendaftaran(id_pendaftaran, no_antrian, id_customer, id_jenis_service, tgl_pendaftaran, jam_pendaftaran, tgl_boking, total_biaya, status) values(NULL,'$no_antrian', '$id_customer', '$id_jenis_service', '$tgl_pendaftaran', '$jam_pendaftaran', '$tgl_boking', '$total_biaya', 'Pendaftaran')";
+        $hasil = mysql_query($query);
+    }
+    // Lihat hasil
+    if ($hasil) {
+        ?>
+        <script language="JavaScript">
+            alert('Pendaftaran Anda Berhasil Dilakukan, Silahkan Datang Ke service Kami');
+            window.location.replace('../index.php');
+        </script>
+    <?php
+    } else {
+        ?>
+        <script language="JavaScript">
+            alert('Pendaftaran Gagal Dilakukan');
+            document.location = '../index.php';
+        </script>
 <?php
-	}
+    }
 }
 ?>
